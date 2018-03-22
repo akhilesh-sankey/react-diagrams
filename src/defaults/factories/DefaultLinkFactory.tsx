374a6dc0774e017698bc8dc20b93e0a7393a7046
Akhilesh <akhilesh.m@sankeysolutions.com>
2018-03-22 16:31:29 +0530

    modified to add arrow head on lines

--- src/defaults/factories/DefaultLinkFactory.tsx
+++ src/defaults/factories/DefaultLinkFactory.tsx
@@ -25,12 +25,22 @@ export class DefaultLinkFactory extends AbstractLinkFactory<DefaultLinkModel> {
 
 	generateLinkSegment(model: DefaultLinkModel, widget: DefaultLinkWidget, selected: boolean, path: string) {
 		return (
-			<path
-				className={selected ? widget.bem("--path-selected") : ""}
-				strokeWidth={model.width}
-				stroke={model.color}
-				d={path}
-			/>
+			<g>
+				<defs>
+					<marker id="arrow" markerWidth="100" markerHeight="15" refX="10" refY="2.7" 
+					orient="auto" markerUnits="strokeWidth" viewBox="0 0 20 20">
+					<path d="M0,0 L0,6 L9,3 z" fill="#f00 !important" />
+					</marker>
+				</defs>
+				<path
+					className={selected ? widget.bem("--path-selected") : ""}
+					fill= "none"
+					strokeWidth={model.width}
+					stroke={model.color}
+					d={path}
+					marker-end="url(#arrow)"
+				/>	
+			</g>
 		);
 	}
 }
